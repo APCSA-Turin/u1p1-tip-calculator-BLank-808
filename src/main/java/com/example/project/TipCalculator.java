1732fc1b5ec160b75bef9302b93d6bcd1a001c0b
Brayan Ajpop <ajpop218@gmail.com>
2024-09-20 12:10:47 +0000

    done

--- src/main/java/com/example/project/TipCalculator.java
+++ src/main/java/com/example/project/TipCalculator.java
@@ -1,20 +1,21 @@
 package com.example.project;
-
+import java.util.Scanner;
 public class TipCalculator {
     //WRITE YOUR PROGRAM IN calculateTip
     public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
         //DO NOT DELETE ANY OF THE CODE BELOW      
         StringBuilder result = new StringBuilder();
-
+        double tip=(int)(((cost*(percent/100.0))*100.0)+.5)/100.0;
+        double costAP= tip+cost;
                        
         result.append("-------------------------------\n");
-        result.append("Total bill before tip: "); //concatenate to this string. DO NOT ERASE AND REWRITE
-        result.append("Total percentage: ");
-        result.append("Total tip: ");
-        result.append("Total Bill with tip: ");
-        result.append("Per person cost before tip: ");
-        result.append("Tip per person: ");
-        result.append("Total cost per person: ");
+        result.append("Total bill before tip: "+"$"+ cost +"\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: "+ percent+"%\n");
+        result.append("Total tip: " +"$"+ tip+ "\n");
+        result.append("Total Bill with tip: "+"$"+ ((tip*100 + cost*100)/100)+ "\n");
+        result.append("Per person cost before tip: "+"$"+((int)(((cost/people)*100.0)+.5)/100.0)+ "\n");
+        result.append("Tip per person: "+"$"+((int)(((tip/people)*100.0)+.5)/100.0)+"\n");
+        result.append("Total cost per person: "+"$"+ ((int)(((costAP/people)*100.0)+.5)/100.0)+"\n");
         result.append("-------------------------------\n");
 
 
@@ -23,9 +24,15 @@ public class TipCalculator {
      //TEST YOUR PROGRAM IN main
      public static void main(String[] args) {
         //try different values for people, percent, and cost to test your program before running test cases
-        int people; 
-        int percent;
-        double cost;              
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Hello");
+        System.out.print ("How many people are paying? ");
+        int people=scan.nextInt(); 
+        System.out.print ("what percent tip are you giving? ");
+        int percent=scan.nextInt();
+        System.out.print ("what the cost of the meal? ");
+        double cost=scan.nextDouble(); 
+        scan.close();             
         System.out.println(calculateTip(people,percent,cost));
     }
 }
